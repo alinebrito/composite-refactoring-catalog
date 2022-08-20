3f3a935fad5534280a1750fae47346306d18bea0
Aline Brito <14023536+alinebrito@users.noreply.github.com>
2022-08-20 07:52:26 -0300

    feat: create readme and svg file for each instance of composite refactoring

--- detector/scripts/graph.py
+++ detector/scripts/graph.py
@@ -36,17 +36,17 @@ class Graph:
         return DG
 
     def get_source_config(self, category):
-        label = 'm'
         fillcolor = 'grey74'
-        if category in ['Class Decomposition']:
-            label = 'c'
+        label = 'm'
+        label = 'c' if category in ['Class Decomposition'] else label
+        label = 'a' if category in ['Push Down Attribute', 'Pull Up Attribute'] else label
         return {'label': label, 'fillcolor': fillcolor }
 
     def get_target_config(self, category):
-        label = 'm'
         fillcolor = 'gray92'
+        label = 'm'
+        label = 'a' if category in ['Push Down Attribute', 'Pull Up Attribute'] else label
         return {'label': label, 'fillcolor': fillcolor }
-
 
     def plot(self, subgraphs, path, category):
         for index, subgraph in enumerate(subgraphs):
@@ -67,8 +67,12 @@ class Graph:
                 svg.edge(edge[0], edge[1], color='dimgrey', href=linkedge, label='{}'.format(properties.get('refactoringType')), fontsize='10', len='0.1', minlen=str(properties.get('weight')), style="dashed")
             label_text = '\n\n{}\n\nSubgraph #{}\n\n\{}'.format(subgraph.graph.get('project'), index, category)
             svg.attr(bgcolor='', label=label_text, fontcolor='black', rankdir='LR', ratio='auto', pad="0.5,0.5")
-            self.save_graph_to_html(svg, subgraph.graph.get('group'), index, path)
-        self.save_graph_to_json(subgraphs, path)    
+            group = subgraph.graph.get('group')
+            file_name = '{}/view/subgraph_{}_{}'.format(path, subgraph.graph.get('group'), index) 
+            self.save_graph_to_html(svg, file_name)
+            self.save_graph_to_svg(svg, file_name)
+            self.create_readme(subgraph, file_name, index)
+        self.save_graph_to_json(subgraphs, path)
         return
 
     def save_graph_to_json(self, subgraphs, path):
@@ -80,14 +84,39 @@ class Graph:
         json.dump(file_json, file)
         file.close()
         pass  
+    
+    def save_graph_to_html(self, svg, file_name):
+        self.save_file(svg,'{}.html'.format(file_name))
+        return
 
-    def save_graph_to_html(self, svg, group, id, path):
-        file_name = '{}/view/subgraph_{}_{}.html'.format(path, group, id) 
+    def save_graph_to_svg(self, svg, file_name):
+        self.save_file(svg,'{}.svg'.format(file_name))
+        pass
+
+    def save_file(self, file, file_name):
         print('Creating {}'.format(file_name))
         os.makedirs(os.path.dirname(file_name), exist_ok=True)
         with io.open(file_name, 'w', encoding='utf8') as f:
-            f.write(svg.pipe().decode('utf-8'))
-        return
+            f.write(file.pipe().decode('utf-8'))
+        pass
+
+    def create_readme(self, graph, file_name, index):
+        json = json_graph.node_link_data(graph)
+        md_file = '{}.md'.format(file_name)
+        svg_file = 'subgraph_{}_{}.svg'.format(graph.graph.get('group'), index)
+        print('Creating {}'.format(md_file))
+        with open(md_file, 'w') as f:
+                f.write("<img src='{}' width='25%'>\n\n".format(svg_file))
+                f.write("## Refactorings:\n")
+                for edge in json.get('links'):
+                    f.write("\n\nid: `{}`\\".format(edge.get('key')))
+                    f.write("\nsource: `{}`\\".format(edge.get('source')))
+                    f.write("\ntarget: `{}`\\".format(edge.get('target')))
+                    f.write("\ntype: `{}`\\".format(edge.get('properties').get('refactoringType')))
+                    f.write("\ncommit: [{}](https://github.com/{}/commit/{})".format(edge.get('properties').get('abbreviatedCommitHash'),json.get('graph').get('project'),edge.get('properties').get('commitHash')))
+                    if edge.get('properties').get('descriptionOracle'):
+                        f.write("\\\ndescription: `{}`".format(edge.get('properties').get('descriptionOracle')))
+        pass
     
 
     
